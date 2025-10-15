11517f9823354631d7852f94678d12ce3115227d
Bruno <brunoturetto@gmail.com>
2025-10-14 22:43:44 -0300

    API 2.6.1: Adicionando Requirements

--- api/database.py
+++ api/database.py
@@ -1,14 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define a URL de conexão para o banco de dados SQLite.
-DATABASE_URL = "sqlite:///./data/livraria.db"
+#habilitando o uso do postgre para utilizar o deploy no render
+
+# Define a URL de conexão para o banco de dados SQLite e PostgreSQL
+DATABASE_URL = os.environ.get('DATABASE_URL', "sqlite:///./data/livraria.db")
 
 # Criar o motor do banco de dados.
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}) 
 
-# Criar uma sessão.
+
+# Criar uma sessão
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Criar base de referência para o SQL
